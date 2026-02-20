fc494eed51712de46e0e6e9510b0fcd63c25647c
DOMIN1310 <poweski2009@gmail.com>
2026-02-20 19:13:41 +0100

    day 3 part 2 recursive + part 1 recursive

--- src/day_3.c
+++ src/day_3.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 static inline int chr_digit_to_int(char chr) {
@@ -7,7 +8,6 @@ static inline int chr_digit_to_int(char chr) {
 }
 
 int get_highest_joltage_two(char* str, size_t len) {
-    // THIS MAY CAUSE ISSUES LATER
     int highest = 0;
 
     char* str_end = str + len;
@@ -30,6 +30,38 @@ int get_highest_joltage_two(char* str, size_t len) {
     return highest;
 }
 
+// expected end held back by the expected length at the end
+static size_t rec_highest_joltage_sub(char* str, char* end, size_t acc) {
+    char* highest_digit = str;
+    for (char* s = str; s < end; ++s) {
+        if ((*s) > (*highest_digit)) {
+            highest_digit = s;
+        }
+    }
+
+    acc = (acc * 10) + chr_digit_to_int(*highest_digit);
+
+    // end the recursion - expected length
+    if ((*end) == '\0') {
+        return acc;
+    }
+
+    // it shifts the window by 1 to the right (gradually goes "back" and "forward")
+    return rec_highest_joltage_sub(highest_digit + 1, end + 1, acc);
+}
+
+size_t rec_get_highest_joltage_12(char* str, size_t len) {
+    // (11 because in the first search we need to find the highest possible value from all 12)
+    char* end = (str + len) - 11;
+    return rec_highest_joltage_sub(str, end, 0);
+}
+
+// recursive version of "get_highest_joltage_two"
+size_t rec_get_highest_joltage_2(char* str, size_t len) {
+    char* end = (str + len) - 1;
+    return rec_highest_joltage_sub(str, end, 0);
+}
+
 int main(void) {
     FILE* f;
     if (fopen_s(&f, "inputs/day_3.txt", "r") != 0) {
@@ -38,6 +70,7 @@ int main(void) {
     }
 
     size_t product_sum = 0;
+    size_t second_sum = 0;
 
     char line[128];
     while (fgets(line, sizeof(line), f)) {
@@ -45,13 +78,17 @@ int main(void) {
         line[strcspn(line, "\r\n")] = '\0';
         size_t line_len = strlen(line);
 
-        // part 1 function
-        product_sum += get_highest_joltage_two(line, line_len);
+        // part 1 function (recursive!)
+        product_sum += rec_get_highest_joltage_2(line, line_len);
+
+        // part 2 function
+        second_sum += rec_get_highest_joltage_12(line, line_len);
     }
 
     fclose(f);
 
     printf("part 1 solution is: %zu\n", product_sum);
+    printf("part 2 solution is: %zu\n", second_sum);
 
     return 0;
 }
