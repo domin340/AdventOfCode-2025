a597ddb08fa001ea1f66e20a1d1eb57db30487e1
DOMIN1310 <poweski2009@gmail.com>
2026-02-20 13:32:41 +0100

    day 3 part 1 in C

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ SOURCE_DIR = src
 BUILD_OUT = bin
 DEBUG_OUT = bin/debug
 
-CURRENT_DAY = day_2
+CURRENT_DAY = day_3
 CURRENT_FILE = $(SOURCE_DIR)/$(CURRENT_DAY).c
 
 day_1:
@@ -13,6 +13,9 @@ day_1:
 
 day_2:
 	$(CC) $(SOURCE_DIR)/day_2.c -o $(BUILD_OUT)/day_2 $(CFLAGS)
+
+day_3:
+	$(CC) $(SOURCE_DIR)/day_3.c -o $(BUILD_OUT)/day_3 $(CFLAGS)
 
 build:
 	$(CC) $(CURRENT_FILE) -o $(BUILD_OUT)/main $(CFLAGS)
---
+++ src/day_3.c
@@ -0,0 +1,57 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+static inline int chr_digit_to_int(char chr) {
+    return chr - '0';
+}
+
+int get_highest_joltage_two(char* str, size_t len) {
+    // THIS MAY CAUSE ISSUES LATER
+    int highest = 0;
+
+    char* str_end = str + len;
+    while (str < str_end) {
+        // looking from left to right this is essentially current digit value times 10
+        int left_product = chr_digit_to_int(*str) * 10;
+
+        for (char* c = (str + 1); c <= str_end; ++c) {
+            // product they create with this pair
+            int product = left_product + chr_digit_to_int(*c);
+
+            if (product > highest) {
+                highest = product;
+            }
+        }
+
+        ++str;
+    }
+
+    return highest;
+}
+
+int main(void) {
+    FILE* f;
+    if (fopen_s(&f, "inputs/day_3.txt", "r") != 0) {
+        perror("couldn't open inputs/day_3.txt file: ");
+        return 1;
+    }
+
+    size_t product_sum = 0;
+
+    char line[128];
+    while (fgets(line, sizeof(line), f)) {
+        // trim line
+        line[strcspn(line, "\r\n")] = '\0';
+        size_t line_len = strlen(line);
+
+        // part 1 function
+        product_sum += get_highest_joltage_two(line, line_len);
+    }
+
+    fclose(f);
+
+    printf("part 1 solution is: %zu\n", product_sum);
+
+    return 0;
+}
